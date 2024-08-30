d82e2434ae6ae0eaa43b9a7a1ed6bb94b7e0c642
brezker <brezker10@gmail.com>
2024-08-29 22:10:43 -0600

    some some tweaks

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,9 +1,42 @@
 extends Node2D
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+@onready var platform_parent = $PlatformParent
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+var camera_scene = preload("res://scenes/game_camera.tscn")
+var platform_scene = preload("res://scenes/platform.tscn")
+
+var camera = null
+
+# Level generation variables
+var start_platform_y
+var y_distance_between_platforms = 100
+
+func _ready():
+	camera = camera_scene.instantiate()
+	camera.setup_camera($Player)
+	add_child(camera)
+
+	# Generar el piso
+	var viewport_size = get_viewport_rect().size
+	var platform_width = 136
+	var ground_layer_platform_count = (viewport_size.x / platform_width) + 1
+	
+	var ground_layer_y_offset = 62
+	for i in range(ground_layer_platform_count):
+		var ground_location = Vector2(i * platform_width, viewport_size.y - ground_layer_y_offset)
+		create_platform(ground_location)
+	
+	# Level Generation
+	
+	
 func _process(delta):
-	pass
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit()
+	if Input.is_action_just_pressed("reset"):
+		get_tree().reload_current_scene()
+
+func create_platform(location: Vector2):
+	var platform = platform_scene.instantiate()
+	platform.global_position = location
+	platform_parent.add_child(platform)
+	return platform
---
+++ scripts/game_camera.gd
@@ -0,0 +1,29 @@
+extends Camera2D
+
+var player: Player = null
+
+var viewport_size
+
+func _ready():
+	# Centrar la pantalla a la mitad del dispositivo
+	viewport_size = get_viewport_rect().size
+	global_position.x = viewport_size.x / 2
+	#print(global_position.x)
+	limit_bottom = viewport_size.y
+	limit_left = 0
+	limit_right = viewport_size.x
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if player:
+		var limit_distance = 420
+		if limit_bottom > player.global_position.y + limit_distance:
+			limit_bottom = player.global_position.y + limit_distance
+
+func setup_camera(_player: Player):
+	if _player != null:
+		player = _player
+		
+func _physics_process(delta):
+	if player:
+		global_position.y = player.global_position.y
